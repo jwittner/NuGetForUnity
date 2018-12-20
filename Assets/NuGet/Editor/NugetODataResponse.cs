9159cca580465673bcca152f4651ab57aa395ce7
Josh Wittner <jowitt@microsoft.com>
2018-12-19 16:29:48 -0800

    WIP!

--- Assets/NuGet/Editor/NugetODataResponse.cs
+++ Assets/NuGet/Editor/NugetODataResponse.cs
@@ -75,12 +75,10 @@ namespace NugetForUnity
                 }
 
                 // Get dependencies
-                package.Dependencies = new List<NugetPackageIdentifier>();
+                package.Dependencies = new List<NugetFrameworkGroup>();
                 string rawDependencies = entryProperties.GetProperty("Dependencies");
                 if (!string.IsNullOrEmpty(rawDependencies))
                 {
-                    var dependencyGroups = new Dictionary<string, NugetFrameworkGroup>();
-
                     string[] dependencies = rawDependencies.Split('|');
                     foreach (var dependencyString in dependencies)
                     {
@@ -101,98 +99,19 @@ namespace NugetForUnity
                             framework = details[2];
                         }
 
-                        NugetFrameworkGroup group;
-                        if (dependencyGroups.TryGetValue(framework, out group))
+                        NugetFrameworkGroup group = package.FindDependencyGroup(framework);
+                        if (group == null)
                         {
-                            group.Dependencies.Add(dependency);
-                        }
-                        else
-                        {
-                            group = new NugetFrameworkGroup();
-                            group.Dependencies = new List<NugetPackageIdentifier>();
-                            group.Dependencies.Add(dependency);
-                            dependencyGroups.Add(framework, group);
-                        }
-                    }
+                            group = new NugetFrameworkGroup()
+                            {
+                                TargetFramework = framework,
+                                Dependencies = new List<NugetPackageIdentifier>()
+                            };
 
-                    // find the correct group for this project
-                    int intDotNetVersion = (int)NugetHelper.DotNetVersion;
-                    //bool using46 = DotNetVersion == ApiCompatibilityLevel.NET_4_6; // NET_4_6 option was added in Unity 5.6
-                    bool using46 = intDotNetVersion == 3; // NET_4_6 = 3 in Unity 5.6 and Unity 2017.1 - use the hard-coded int value to ensure it works in earlier versions of Unity
-                    NugetFrameworkGroup selectedGroup = null;
+                            package.Dependencies.Add(group);
+                        }
 
-                    foreach (var kvPair in dependencyGroups.OrderByDescending(x => x.Key))
-                    {
-                        string framework = kvPair.Key;
-                        NugetFrameworkGroup group = kvPair.Value;
-
-                        // Select the highest .NET library available that is supported
-                        // See here: https://docs.nuget.org/ndocs/schema/target-frameworks
-                        if (using46 && framework == "net462")
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (using46 && framework == "net461")
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (using46 && framework == "net46")
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (using46 && framework == "net452")
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (using46 && framework == "net451")
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (using46 && framework == "net45")
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (using46 && framework == "net403")
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (using46 && (framework == "net40" || framework == "net4"))
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (framework == "net35")
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (framework == "net20")
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (framework == "net11")
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                        else if (framework == string.Empty)
-                        {
-                            selectedGroup = group;
-                            break;
-                        }
-                    }
-
-                    if (selectedGroup != null)
-                    {
-                        package.Dependencies = selectedGroup.Dependencies;
+                        group.Dependencies.Add(dependency);
                     }
                 }
 
